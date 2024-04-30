03ef3120e55eee0fa48589efd685b1f90250a9b6
Sudhir Sreedharan <sudhir@alifsemi.com>
2024-04-30 18:46:58 +0530

    samples: drivers: spi_dw: refactor the application
    - DMA0 is set as default for SPI transfers
    - Supported the below modes
        - Master Transmit
        - Master Receive
        - Slave Transmit
        - Slave Receive
    
    The master thread will send predefined Tx Data to slave(SPI0)
    and similarly Slave will send Tx Data to Master. The result
    will be displayed in the console.
    
    Signed-off-by: Sudhir Sreedharan <sudhir@alifsemi.com>

--- samples/drivers/spi_dw/src/main.c
+++ samples/drivers/spi_dw/src/main.c
@@ -20,7 +20,6 @@ LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
 
 #define Mhz		1000000
 #define khz		1000
-#define BUFF_SIZE	5
 
 /* master_spi and slave_spi aliases are defined in
  * overlay files to use different SPI instance if needed.
@@ -34,10 +33,11 @@ LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
 #define STACKSIZE 1024
 
 /* scheduling priority used by each thread */
-#define PRIORITY 7
+#define MASTER_PRIORITY 7
+#define SLAVE_PRIORITY 6
 
 /* delay between greetings (in ms) */
-#define SLEEPTIME 500
+#define SLEEPTIME 1000
 
 K_THREAD_STACK_DEFINE(MasterT_stack, STACKSIZE);
 static struct k_thread MasterT_data;
@@ -45,101 +45,98 @@ static struct k_thread MasterT_data;
 K_THREAD_STACK_DEFINE(SlaveT_stack, STACKSIZE);
 static struct k_thread SlaveT_data;
 
-void test_spi_transceive(const struct device *dev,
-			struct spi_cs_control *cs)
-{
-	struct spi_config cnfg;
-	int length;
-	cnfg.frequency = 1 * Mhz;
-	cnfg.operation = SPI_OP_MODE_MASTER | SPI_WORD_SET(32);
-	printk("transceive operation %x\n", cnfg.operation);
-	cnfg.slave = 0;
-	cnfg.cs = cs;
+/* Master and Slave buffer size */
+#define BUFF_SIZE  200
 
-	uint32_t txdata[BUFF_SIZE] = {0xFFFFFFFF, 0xFFFFFFFE,
-					 0xFFFFFFFD, 0xFFFFFFFC, 0xFFFFFFFB};
-	uint32_t rxdata[BUFF_SIZE];
-	length = BUFF_SIZE * sizeof(txdata[0])	;
+/* Master and Slave buffer word size */
+#define SPI_WORD_SIZE 8
 
-	struct spi_buf tx_buffer[1] = {
-		{
-			.buf = txdata,
-			.len = length
-		},
-	};
-	struct spi_buf rx_buffer[1] = {
-		{
-			.buf = rxdata,
-			.len = length
-		},
-	};
-	struct spi_buf_set tx_set = {
-		.buffers = &tx_buffer[0],
-		.count = 1
-	};
-	struct spi_buf_set rx_set = {
-		.buffers = &rx_buffer[0],
-		.count = 1
-	};
+/* Master and Slave buffer frequency */
+#define SPI_FREQUENCY (1 * Mhz)
 
-	int ret = spi_transceive(dev, &cnfg, &tx_set, &rx_set);
+/* Master and Slave buffer transfers */
+#define SPI_NUM_TRANSFERS  10
 
-	printk("SPI Transceive function called with return : %d\n", ret);
-	printk(" tx (i)  : %08x %08x %08x %08x %08x\n",
-	       txdata[0], txdata[1], txdata[2], txdata[3], txdata[4]);
-	printk(" rx (i)  : %08x %08x %08x %08x %08x\n",
-	       rxdata[0], rxdata[1], rxdata[2], rxdata[3], rxdata[4]);
-}
+/* Master and Slave buffers */
+static uint32_t master_txdata[BUFF_SIZE];
+static uint32_t master_rxdata[BUFF_SIZE];
+static uint32_t slave_txdata[BUFF_SIZE];
+static uint32_t slave_rxdata[BUFF_SIZE];
 
-void test_spi_receive(const struct device *dev,
+/*
+ * Send/Receive data through slave spi
+ */
+int slave_spi_transceive(const struct device *dev,
 				 struct spi_cs_control *cs)
 {
 	struct spi_config cnfg;
+	int ret;
 
-	cnfg.frequency = 1 * Mhz;
-	cnfg.operation = SPI_OP_MODE_SLAVE | SPI_WORD_SET(16);
+	cnfg.frequency = SPI_FREQUENCY;
+	cnfg.operation = SPI_OP_MODE_SLAVE | SPI_WORD_SET(SPI_WORD_SIZE);
 	cnfg.slave = 0;
 	cnfg.cs = NULL;
 
-	static uint32_t rxdata[BUFF_SIZE];
-	int length = (BUFF_SIZE) * sizeof(rxdata[0]);
+	int length = (BUFF_SIZE) * sizeof(slave_rxdata[0]);
 
 	struct spi_buf rx_buf = {
-		.buf = rxdata,
+		.buf = slave_rxdata,
 		.len = length
 	};
 	struct spi_buf_set rx_bufset = {
 		.buffers = &rx_buf,
 		.count = 1
 	};
+	struct spi_buf tx_buf = {
+		.buf = slave_txdata,
+		.len = length
+	};
+	struct spi_buf_set tx_bufset = {
+		.buffers = &tx_buf,
+		.count = 1
+	};
 
-	int ret = spi_transceive(dev, &cnfg, NULL, &rx_bufset);
+	ret = spi_transceive(dev, &cnfg, &tx_bufset, &rx_bufset);
+	if (ret < 0) {
+		printk("ERROR: Slave SPI Transceive: %d\n", ret);
+	}
 
-	printk("test SPI read only returns: %d\n", ret);
+	printk("slave wrote: %08x %08x %08x %08x %08x\n",
+		slave_txdata[0], slave_txdata[1], slave_txdata[2],
+		slave_txdata[3], slave_txdata[4]);
+
 	printk("slave read: %08x %08x %08x %08x %08x\n",
-		rxdata[0], rxdata[1], rxdata[2], rxdata[3], rxdata[4]);
+		slave_rxdata[0], slave_rxdata[1], slave_rxdata[2],
+		slave_rxdata[3], slave_rxdata[4]);
+
+	ret = memcmp(master_txdata, slave_rxdata, length);
+	if (ret) {
+		printk("ERROR: SPI Master TX & Slave RX DATA NOT MATCHING: %d\n", ret);
+	} else {
+		printk("SUCCESS: SPI Master TX & Slave RX DATA IS MATCHING: %d\n", ret);
+	}
+
+	return ret;
 }
 
 /*
- * Tests SPI write only.
- * Passing only tx buffer, Output needs to be verified with a logic analyzer.
+ * Send/Receive data through master spi
  */
-void test_spi_transmit(const struct device *dev,
+int master_spi_transceive(const struct device *dev,
 				 struct spi_cs_control *cs)
 {
 	struct spi_config cnfg;
+	int ret;
 
-	cnfg.frequency = 1 * Mhz;
-	cnfg.operation = SPI_OP_MODE_MASTER | SPI_WORD_SET(16);
+	cnfg.frequency = SPI_FREQUENCY;
+	cnfg.operation = SPI_OP_MODE_MASTER | SPI_WORD_SET(SPI_WORD_SIZE);
 	cnfg.slave = 0;
 	cnfg.cs = cs;
 
-	static uint32_t txdata[BUFF_SIZE] = { 0xFFFF1111, 0xFFFF2222,
-					0xFFFF3333, 0xFFFF4444, 0xFFFF5555};
-	int length = (BUFF_SIZE) * sizeof(txdata[0]);
+	int length = (BUFF_SIZE) * sizeof(master_txdata[0]);
 
 	struct spi_buf tx_buf = {
-		.buf = txdata,
+		.buf = master_txdata,
 		.len = length
 	};
 
@@ -148,16 +145,41 @@ void test_spi_transmit(const struct device *dev,
 		.count = 1
 	};
 
-	int ret = spi_transceive(dev, &cnfg, &tx_bufset, NULL);
+	struct spi_buf rx_buf = {
+		.buf = master_rxdata,
+		.len = length
+	};
+	struct spi_buf_set rx_bufset = {
+		.buffers = &rx_buf,
+		.count = 1
+	};
 
-	printk("test SPI write only returns: %d\n", ret);
+	ret = spi_transceive(dev, &cnfg, &tx_bufset, &rx_bufset);
+	if (ret) {
+		printk("ERROR: SPI=%p transceive: %d\n", dev, ret);
+	}
 	printk("Master wrote: %08x %08x %08x %08x %08x\n",
-		txdata[0], txdata[1], txdata[2], txdata[3], txdata[4]);
+		master_txdata[0], master_txdata[1], master_txdata[2],
+		master_txdata[3], master_txdata[4]);
+	printk("Master receive: %08x %08x %08x %08x %08x\n",
+		master_rxdata[0], master_rxdata[1], master_rxdata[2],
+		master_rxdata[3], master_rxdata[4]);
+
+	ret = memcmp(master_rxdata, slave_txdata, length);
+	if (ret) {
+		printk("ERROR: SPI Master RX & Slave TX DATA NOT MATCHING: %d\n", ret);
+	} else {
+		printk("SUCCESS: SPI Master RX & Slave TX DATA IS MATCHING: %d\n", ret);
+	}
+
+	return ret;
 }
 
 static void master_spi(void *p1, void *p2, void *p3)
 {
 	const struct device *const dev = DEVICE_DT_GET(SPIDW_NODE);
+	uint32_t iterations = SPI_NUM_TRANSFERS;
+	int ret;
 
 	ARG_UNUSED(p1);
 	ARG_UNUSED(p2);
@@ -171,16 +193,24 @@ static void master_spi(void *p1, void *p2, void *p3)
 		.gpio = GPIO_DT_SPEC_GET(SPIDW_NODE, cs_gpios),
 		.delay = 0u,
 	};
-	while (1) {
-		printk("Master Transmit\n");
-		test_spi_transmit(dev, &cs_ctrl);
+	while (iterations) {
+		printk("Master Transceive Iter= %d\n", iterations);
+		ret = master_spi_transceive(dev, &cs_ctrl);
 		k_msleep(SLEEPTIME);
+		if (ret < 0) {
+			printk("Stopping the Master Thread due to error\n");
+			return;
+		}
+		iterations--;
 	}
+	printk("Master Transfer Successfully Completed\n");
 }
 
 static void slave_spi(void *p1, void *p2, void *p3)
 {
 	const struct device *const slave_dev = DEVICE_DT_GET(S_SPIDW_NODE);
+	uint32_t iterations = SPI_NUM_TRANSFERS;
+	int ret;
 
 	ARG_UNUSED(p1);
 	ARG_UNUSED(p2);
@@ -194,10 +224,16 @@ static void slave_spi(void *p1, void *p2, void *p3)
 		.gpio = GPIO_DT_SPEC_GET(S_SPIDW_NODE, cs_gpios),
 		.delay = 0u,
 	};
-	while (1) {
-		printk("slave Receive\n");
-		test_spi_receive(slave_dev, &slave_cs_ctrl);
+	while (iterations) {
+		printk("Slave Transceive Iter= %d\n", iterations);
+		ret = slave_spi_transceive(slave_dev, &slave_cs_ctrl);
+		if (ret < 0) {
+			printk("Stopping the Slave Thread due to error\n");
+			return;
+		}
+		iterations--;
 	}
+	printk("Slave Transfer Successfully Completed\n");
 }
 
 #if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(dma2), arm_dma_pl330, okay)
@@ -285,6 +321,14 @@ static void configure_spi0_lpspi_for_dma0(void)
 	sys_write32(regdata, EVTRTR0_DMA_ACK_TYPE0 + (SPI0_DMA_GROUP * 4));
 }
 #endif
+
+static void prepare_data(uint32_t *data, uint16_t def_mask)
+{
+	for (uint32_t cnt = 0; cnt < BUFF_SIZE; cnt++) {
+		data[cnt] = (def_mask << 16) | cnt;
+	}
+}
+
 void main(void)
 {
 #if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(dma2), arm_dma_pl330, okay)
@@ -293,15 +337,18 @@ void main(void)
 	configure_spi0_lpspi_for_dma0();
 #endif
 
+	prepare_data(master_txdata, 0xFFFF);
+	prepare_data(slave_txdata, 0xAAAA);
+
 	k_tid_t tids = k_thread_create(&SlaveT_data, SlaveT_stack, STACKSIZE,
 			&slave_spi, NULL, NULL, NULL,
-			PRIORITY, 0, K_NO_WAIT);
+			SLAVE_PRIORITY, 0, K_NO_WAIT);
 	if (tids == NULL) {
 		printk("Error creating Slave Thread\n");
 	}
 	k_tid_t tidm = k_thread_create(&MasterT_data, MasterT_stack, STACKSIZE,
 			&master_spi, NULL, NULL, NULL,
-			PRIORITY, 0, K_NO_WAIT);
+			MASTER_PRIORITY, 0, K_NO_WAIT);
 	if (tidm == NULL) {
 		printk("Error creating Master Thread\n");
 	}
